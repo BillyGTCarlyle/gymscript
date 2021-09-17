520db1aa879bcef5b2bf9dbe12287e4e2fbac395
Billy Carlyle <billy@billycarlyle.uk>
2021-09-17 22:23:07 +0100

    Fixed digit issue
    Previous version wasn't working for double digits.

--- gym.sh
+++ gym.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-read value < <(curl -s https://www.st-andrews.ac.uk/sport/ | gawk 'match($0, /Occupancy: (.)%/, a) {print a[1]}')
+read value < <(curl -s https://www.st-andrews.ac.uk/sport/ | gawk 'match($0, /Occupancy: [^%]*/, a) {print a[0]}' | grep -Eo '[0-9]{1,3}')
 echo "Gym occupancy: $value%"
 
 
